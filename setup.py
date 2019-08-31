7cf1bc327a254c5126b531b21053f0c7522added
Stephen Moore <stephen@delfick.com>
2019-08-31 14:27:44 +1000

    Initial commit

---
+++ setup.py
@@ -0,0 +1,31 @@
+from noy_black import VERSION
+from setuptools import setup
+
+# fmt: off
+setup(
+      name = 'noy_black'
+    , version = VERSION
+    , packages = ["noy_black"]
+
+    , python_requires = ">= 3.6"
+
+    , install_requires =
+      [ 'black'
+      , 'noseOfYeti==1.8.3'
+      ]
+
+    , entry_points =
+      { 'console_scripts' :
+        [ 'noy_black = noy_black.main:main'
+        ]
+      }
+
+    , author = 'Stephen Moore'
+    , license = 'MIT'
+    , author_email = 'delfick755@gmail.com'
+
+    , url = "https://github.com/delfick/noy_black"
+    , description = 'Monkey patched black that understands nose of yeti'
+    , long_description = open("README.rst").read()
+    )
+# fmt: on
